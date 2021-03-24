6720122fc5fdab612aa753679a333a78cfb1151b
nicholaskier <nicholaskier@gmail.com>
2021-03-24 16:13:26 -0500

    adding mult points

--- map1.py
+++ map1.py
@@ -1,6 +1,9 @@
 import folium
 map = folium.Map(location=[30.322016, -97.681918], zoom_start=6, titles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi! I am a marker!", icon=folium.Icon(color="green")))
+
+for coordinates in [[38.2, -99.1],[37.2, -99.1]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi! I am a marker!", icon=folium.Icon(color="green")))
+
 map.add_child(fg)
 map.save("Map1.html")
