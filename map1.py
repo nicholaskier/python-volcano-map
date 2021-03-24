6a2e31dec81f40e7b6e0f1371551f7484b02e335
nicholaskier <nicholaskier@gmail.com>
2021-03-24 16:28:25 -0500

    add volcanoes

--- map1.py
+++ map1.py
@@ -1,9 +1,16 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[30.322016, -97.681918], zoom_start=6, titles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1],[37.2, -99.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi! I am a marker!", icon=folium.Icon(color="green")))
+for lt, ln in zip(lat, lon):
+    # zip function distributes items one by one from each list
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi! I am a volcano!", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 map.save("Map1.html")
