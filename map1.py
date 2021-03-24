385433b60199490a67c739ddf87145dd8a02b15a
nicholaskier <nicholaskier@gmail.com>
2021-03-24 17:04:54 -0500

    add detailed folium popup

--- map1.py
+++ map1.py
@@ -5,13 +5,21 @@ data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+name = list(data["NAME"])
+
+html = """
+Volacano name:<br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+Height = %s m
+"""
 
 map = folium.Map(location=[30.322016, -97.681918], zoom_start=6, titles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln, el in zip(lat, lon, elev):
+for lt, ln, el, name in zip(lat, lon, elev, name):
     # zip function distributes items one by one from each list
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Elevation: " + str(el) + "m", icon=folium.Icon(color="green")))
+    iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 map.save("Map1.html")
