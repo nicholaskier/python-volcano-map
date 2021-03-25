5b3c286e476b942357cdbfde7abbdee5ae788601
nicholaskier <nicholaskier@gmail.com>
2021-03-25 15:03:39 -0500

    add layer controls

--- map1.py
+++ map1.py
@@ -22,15 +22,22 @@ Height = %s m
 """
 
 map = folium.Map(location=[30.322016, -97.681918], zoom_start=6, titles="Stamen Terrain")
-fg = folium.FeatureGroup(name="My Map")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
     # zip function distributes items one by one from each list
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=9, popup=folium.Popup(iframe), fill_color=color_producer(el), color="white", fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=9, popup=folium.Popup(iframe), fill_color=color_producer(el), color="white", fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open("world.json", 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open("world.json", 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 # add polygon layers from world.json file
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
+
 map.save("Map1.html")
